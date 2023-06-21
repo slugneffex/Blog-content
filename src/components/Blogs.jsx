e4fcae76c0e2395aea80504bfca3b3d8f85573ca
slugneffex <rawatmayank969@gmail.com>
2023-06-21 15:29:43 +0530

    project completed

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -5,7 +5,7 @@ import Spinner from './Spinner'
 const Blogs = () => {
     const { loading, posts } = useContext(AppContext)
     return (
-        <div>
+        <div className='w-11/12 max-w-[650px] h-screen py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px] justify-center items-center'>
             {
                 loading ? (
                     <Spinner />
@@ -19,15 +19,15 @@ const Blogs = () => {
                         (
                             posts.map((post) => (
                                 <div>
-                                    <p className='font-bold'>{post.title}</p>
-                                    <p>
-                                        By <span>{post.author}</span> on <span>{post.category}</span>
+                                    <p className='font-bold text-lg'>{post.title}</p>
+                                    <p className='text-sm mt-[4px]'>
+                                        By <span className='italic'>{post.author}</span> on <span className='underline font-bold'>{post.category}</span>
                                     </p>
-                                    <p>Posted on {post.date}</p>
-                                    <p>{post.content}</p>
-                                    <div>
+                                    <p className='text-sm mt-[4px]'>Posted on {post.date}</p>
+                                    <p className='text-md mt-[14px]'>{post.content}</p>
+                                    <div className='flex gap-x-3'>
                                         {post.tags.map((tag) => {
-                                            return <span>{`#$(tag)`}</span>
+                                            return <span className='text-blue-700 undline font-bold text-xs mt-[5px]'>{`#$(tag)`}</span>
                                         })}
                                     </div>
                                 </div>
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,24 +4,30 @@ import { useContext } from 'react'
 
 const Pagination = () => {
 
-    const {page, handlePageChange, totalPages} = useContext(AppContext)
+  const { page, handlePageChange, totalPages } = useContext(AppContext)
   return (
-    <div>
-      <div>
-        { page > 1 &&
-            <button onClick={() => handlePageChange(page-1)}>
-                Previous
-            </button>
-        }
-        
-        { page < totalPages &&
-            <button onClick={() => handlePageChange(page+1)}>
-                Next
-            </button>
-        }
+    <div className='w-full flex justify-center items-center border-2 fixed bottom-0 bg-white'>
+      <div className='flex justify-between w-11/12 max-w-[670px] py-2'>
+        <div className='flex gap-x-2'>
+          {page > 1 &&
+            (<button
+              className='rounded-md border-2 px-4 py-1'
+              onClick={() => handlePageChange(page - 1)}>
+              Previous
+            </button>)
+          }
 
-        <p>
-            Page {page} of {totalPages}
+          {page < totalPages &&
+            (<button
+              className='rounded-md border-2 px-4 py-1'
+              onClick={() => handlePageChange(page + 1)}>
+              Next
+            </button>)
+          }
+        </div>
+
+        <p className='font-bold text-sm'>
+          Page {page} of {totalPages}
         </p>
       </div>
     </div>
